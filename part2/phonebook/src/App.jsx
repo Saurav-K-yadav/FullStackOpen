de2a216d1df1f582bb7d93872409dff3cc6ac934
Saurav Kumar Yadav <sauravkumaryadav100@gmail.com>
2024-01-01 16:42:59 +0530

    2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,21 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios';
 import Inp from "./components/Inp"
 import Numbers from "./components/numbers";
 import SearchContact from "./components/contact";
 
 
 const App = () => {
-    const [persons, setPersons] = useState([
-      { name: "Arto Hellas", number: "040-123456", id: 1 },
-      { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-      { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-      { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-    ]);
-  const [searchResult, setSearchResult] = useState("");
+   const [persons, setPersons] = useState([]);
+   const [searchResult, setSearchResult] = useState("");
+   const [newName, setNewName] = useState("");
+   const [newNumber, setNewNumber] = useState("");
+  
+  useEffect(() => {
+    console.log('Fetching')
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response) => {
+        setPersons(response.data);
+      }
 
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-
+      )
+  }
+  , []);
+ 
   const changeName = function (event) {
     console.log(event.target.value);
     setNewName(event.target.value);
