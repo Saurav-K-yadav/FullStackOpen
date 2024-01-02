0f42f83817afd690ff37f7e9f9156123924091cf
Saurav Kumar Yadav <sauravkumaryadav100@gmail.com>
2024-01-02 21:59:43 +0530

    added css

--- part2/exercises/src/App.jsx
+++ part2/exercises/src/App.jsx
@@ -1,45 +1,65 @@
 import Note from "./components/note";
 import { useState, useEffect } from "react";
-import axios from "axios";
+import Notification from "./components/notification";
 import noteService from "./services/notes";
 
+const Footer = () => {
+  const footerStyle = {
+    color: "green",
+    fontStyle: "italic",
+    fontSize: 16,
+  };
+  return (
+    <div style={footerStyle}>
+      <p>
+        Note app, Made By Saurav Kumar Yadav<br></br>
+      </p>
+      <a href="https://github.com/Saurav-K-yadav/Saurav-K-yadav/tree/main">
+        github: Saurav-K-yadav
+      </a>
+    </div>
+  );
+};
+
 const App = () => {
-  const [notes,setNotes]=useState([])
-  const [newNote, setNewNote] = useState("a new note..."); 
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("some error happened...");
 
-useEffect(() => {
-  noteService.getAll().then((response) => {
-    setNotes(response);
-  });
-}, []);
+  useEffect(() => {
+    noteService.getAll().then((response) => {
+      setNotes(response);
+    });
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 5000);
+  }, []);
 
-  const notesToShow = showAll
-    ? notes
-    : notes.filter((note) => note.important );
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
   const addNotes = (event) => {
     event.preventDefault();
     const noteObject = {
       content: newNote,
-      id:notes.length + 1,
-      important:Math.random()<0.5,
-    }
-    
-   noteService.create(noteObject).then((response) => {
-     console.log(response);
-     setNotes(notes.concat(response));
-     setNewNote('');
-   });
+      id: notes.length + 1,
+      important: Math.random() < 0.5,
     };
 
- const handleNoteChange = (event) => {
-   console.log(event.target.value);
-   setNewNote(event.target.value);
- };
+    noteService.create(noteObject).then((response) => {
+      console.log(response);
+      setNotes(notes.concat(response));
+      setNewNote("");
+    });
+  };
 
-  const toggleImportance = (id) => { 
-    console.log(`${id} needs to be toggled important`)
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const toggleImportance = (id) => {
+    console.log(`${id} needs to be toggled important`);
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
     noteService
@@ -48,31 +68,48 @@ useEffect(() => {
         setNotes(notes.map((n) => (n.id !== id ? n : response)));
       })
       .catch((error) => {
-        alert(`the note '${note.content}' was already deleted from server`);
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
         setNotes(notes.filter((n) => n.id !== id));
       });
-  }
+  };
 
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
+
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} toggleImportance={()=> toggleImportance(note.id)} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportance(note.id)}
+          />
         ))}
       </ul>
       <ul>
-        <button onClick={() => { setShowAll(!showAll)}}>Show{ showAll?' Important':' All'}</button>
+        <button
+          onClick={() => {
+            setShowAll(!showAll);
+          }}
+        >
+          Show{showAll ? " Important" : " All"}
+        </button>
       </ul>
       <ul>
         <form onSubmit={addNotes}>
-          <input value={newNote} onChange={handleNoteChange} required/>
+          <input value={newNote} onChange={handleNoteChange} required />
           <button type="submit">Save</button>
         </form>
       </ul>
+      <Footer />
     </div>
   );
 };
 
 export default App;
-
--- part2/exercises/src/components/note.jsx
+++ part2/exercises/src/components/note.jsx
@@ -1,6 +1,6 @@
 const Note = ({ note,toggleImportance }) => {
 const label=note.important?'Mark Not Important ':"Mark Important"
-  return (<li>{note.content}
+  return (<li className="note">{note.content}
     <button onClick={toggleImportance}> {label } </button>
   </li>)
 };
