a2089e43863461decef1c86e2a5f8db07e5464d3
Saurav Kumar Yadav <sauravkumaryadav100@gmail.com>
2024-01-29 15:46:25 +0530

    integrated database

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,35 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Note = require('./models/notes')
+
+const mongoose = require('mongoose')
+
+// const password = process.argv[2]
+
+// const url =
+//   `mongodb+srv://sauravkumaryadav100:${password}@cluster0.aphlczd.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+// mongoose.set('strictQuery', false)
+// mongoose.connect(url)
+
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   important: Boolean,
+// })
+
+// noteSchema.set('toJSON', {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString()
+//     delete returnedObject._id
+//     delete returnedObject.__v
+//   }
+// })
+
+
+
+// const Note = mongoose.model('Note', noteSchema)
 
 app.use(cors())
 app.use(express.static('dist'))
@@ -27,17 +56,6 @@ let notes = [
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
@@ -47,20 +65,21 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
-
-  notes = notes.concat(note)
-
-  response.json(note)
+  })
+  note.save().then(savedNotes => { 
+    response.json(savedNotes)
+  })
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)    
+  })
+
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -71,14 +90,17 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
+  // const id = Number(request.params.id)
+  // const note = notes.find(note => note.id === id)
+
+  // if (note) {
+  //   response.json(note)
+  // } else {
+  //   response.status(404).end()
+  // }
 })
 
 const PORT = process.env.PORT || 3001
