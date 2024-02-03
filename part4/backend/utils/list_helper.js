aa4bcda8938305ef0e7260a34646c97aa5a33cdd
Saurav Kumar Yadav <sauravkumaryadav100@gmail.com>
2024-02-03 19:27:19 +0530

    4.7: Most liked Atuhor

--- part4/backend/utils/list_helper.js
+++ part4/backend/utils/list_helper.js
@@ -9,6 +9,24 @@ const total_likes = (blogs) => {
 return blogs.reduce(reduce,0)
 }
 
+const mostLikes = (blogs) => {
+    author = ""
+    if (!blogs) {
+        return { author: `${author}`, likes: '0' }
+    }
+    const reducer = (prev, curr) => {
+        if (curr.likes >= prev) {
+            prev = curr.likes
+            author=curr.author
+        }
+        return prev
+    }
+    const mostLiked = blogs.reduce(reducer, 0)
+    return ({ author: `${author}`, likes: `${mostLiked}` })
+}
+
 module.exports = {
-    dummy,total_likes
+    dummy,
+    total_likes,
+    mostLikes
 }
