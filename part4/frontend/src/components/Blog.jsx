a84ab5e3b526161f8a1671b40c08b52cc540dcde
Saurav Kumar Yadav <sauravkumaryadav100@gmail.com>
2024-02-27 17:04:03 +0530

    5.6 & 5.7

--- part4/frontend/src/components/Blog.jsx
+++ part4/frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [view,setView]=useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+const hideDetails = { display:view?'':'none'}
+  const toggler = () => {
+  setView(!view)
+}
+  const viewbtn = {display:view?'none':''}
+  return (
+  <div style={blogStyle}>
+      {blog.title} {'   '}
+      <button onClick={toggler} style={viewbtn}>View Details</button>
+      <div style={hideDetails}>
+        Author : {blog.author}
+        <br />
+        URl : {blog.url}
+       <br />
+        Likes : {blog.likes} 
+        <br />
+      <button onClick={toggler}>hideDetails</button>
+      </div>
   </div>  
-)
+)}
 
 export default Blog
