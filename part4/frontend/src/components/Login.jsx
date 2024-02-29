a7fca62ab83f98bdd7f3c2d213dd0bcdc137d2e5
Saurav Kumar Yadav <sauravkumaryadav100@gmail.com>
2024-02-29 13:19:10 +0530

    5.12

--- part4/frontend/src/components/Login.jsx
+++ part4/frontend/src/components/Login.jsx
@@ -1,30 +1,39 @@
+import PropTypes from "prop-types";
+
 const LoginForm = ({
   handleSubmit,
   handleUsernameChange,
   handlePasswordChange,
   username,
-  password
+  password,
 }) => {
-    return (
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
         <div>
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    Username 
-                    <input type="text"
-                        value={username}
-                        onChange={handleUsernameChange}
-                    />
-                </div>
-                <div>
-                    Password 
-                    <input type="password"
-                        value={password}
-                    onChange={handlePasswordChange}/>
-                </div>
-                <button type="submit">Login</button>
-            </form>
+          Username
+          <input type="text" value={username} onChange={handleUsernameChange} />
         </div>
-    )
-}
-export default LoginForm
+        <div>
+          Password
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+LoginForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+};
+export default LoginForm;
