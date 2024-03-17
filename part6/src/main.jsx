e864450ef7c689d1213372a4a6c82687040a38ab
Saurav Kumar Yadav <sauravkumaryadav100@gmail.com>
2024-03-17 12:41:18 +0530

    added store reducers

--- part6/src/main.jsx
+++ part6/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from './App.jsx'
 
+import noteReducer from "./reducers/noteReducer.jsx";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
+const store = createStore(noteReducer);
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
 )
--- part6/src/reducers/noteReducer.jsx
+++ part6/src/reducers/noteReducer.jsx
@@ -1,4 +1,4 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
 
 const noteReducer = (state = [], action) => {
   switch (action.type) {
@@ -19,24 +19,25 @@ const noteReducer = (state = [], action) => {
   }
 };
 
-const store = createStore(noteReducer);
 
-store.dispatch({
-  type: "NEW_NOTE",
-  payload: {
-    content: "the app state is in redux store",
-    important: true,
-    id: 1,
-  },
-});
+const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
-store.dispatch({
-  type: "NEW_NOTE",
-  payload: {
-    content: "state changes are made with actions",
-    important: false,
-    id: 2,
-  },
-});
+export const createNote = (content) => {
+  return {
+    type: "NEW_NOTE",
+    payload: {
+      content,
+      important: false,
+      id: generateId(),
+    },
+  };
+};
+
+export const toggleImportanceOf = (id) => {
+  return {
+    type: "TOGGLE_IMPORTANCE",
+    payload: { id },
+  };
+};
 
 export default noteReducer;
