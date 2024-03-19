7c92d26e8408c4bb294880e1c73e61da6590343a
Saurav Kumar Yadav <sauravkumaryadav100@gmail.com>
2024-03-19 15:55:20 +0530

    used reduxToolkit for peace of mind

--- part6/src/main.jsx
+++ part6/src/main.jsx
@@ -2,35 +2,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { createStore ,combineReducers} from "redux";
+import { configureStore} from '@reduxjs/toolkit'
 import { Provider } from "react-redux";
 
 import App from "./App";
 import noteReducer from "./reducers/noteReducer";
 import filterReducer from "./reducers/filterReducer";
-import { createNote } from "./reducers/noteReducer";
-import { filterChange } from "./reducers/filterReducer";
+ 
 
-const reducer = combineReducers({
+const store = configureStore({
+reducer:{
   notes: noteReducer,
-  filter:filterReducer
+  filter:filterReducer}
 })
 
-const store = createStore(reducer);
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>
-// );
+console.log(store.getState())
+ 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
   </Provider>
 );
 
-// store.subscribe(() => console.log(store.getState()))
-// store.dispatch(filterChange('IMPORTANT'))
-// store.dispatch(
-//   createNote("combineReducers forms one reducer from many simple reducers")
-// );
--- part6/src/reducers/noteReducer.test.js
+++ part6/src/reducers/noteReducer.test.js
@@ -1,26 +1,24 @@
+
 import noteReducer from './noteReducer'
 import deepFreeze from 'deep-freeze'
 
 describe('noteReducer', () => {
-    test('returns new state with action NEW_NOTE', () => {
+    test('returns new state with action notes/createNote', () => {
         const state = []
         const action = {
-            type: 'NEW_NOTE',
-            payload: {
-                content: 'the app state is in redux store',
-                important: true,
-                id: 1
-            }
+
+            type: 'notes/createNote',
+            payload: 'the app state is in redux store',
         }
 
         deepFreeze(state)
         const newState = noteReducer(state, action)
 
         expect(newState).toHaveLength(1)
-        expect(newState).toContainEqual(action.payload)
+        expect(newState.map(s => s.content)).toContainEqual(action.payload)
     })
 
-    test('return new state with TOGGLE_IMPORTANCE', () => {
+    test('returns new state with action notes/toggleImportanceOf', () => {
         const state = [
             {
                 content: 'the app state is in redux store',
@@ -31,22 +29,25 @@ describe('noteReducer', () => {
                 content: 'state changes are made with actions',
                 important: false,
                 id: 2
-            }    
-        ]
+            }]
+
         const action = {
-            type: "TOGGLE_IMPORTANCE",
-            payload: {
-                id:2
-            }
+
+            type: 'notes/toggleImportanceOf',
+            payload: 2
         }
+
         deepFreeze(state)
-        const newState=noteReducer(state,action)
+        const newState = noteReducer(state, action)
+
         expect(newState).toHaveLength(2)
+
         expect(newState).toContainEqual(state[0])
+
         expect(newState).toContainEqual({
             content: 'state changes are made with actions',
             important: true,
-            id: 2 
+            id: 2
         })
     })
 })
