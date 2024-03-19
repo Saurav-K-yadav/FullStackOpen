bc23690798195ff624d1fa2a6edba4e3810e4acf
Saurav Kumar Yadav <sauravkumaryadav100@gmail.com>
2024-03-19 12:33:42 +0530

    display only selected fields

--- part6/src/reducers/noteReducer.jsx
+++ part6/src/reducers/noteReducer.jsx
@@ -1,15 +1,29 @@
+const initialState = [
+  {
+    content: "reducer defines how redux store works",
+    important: true,
+    id: 1,
+  },
+  {
+    content: "state of store can contain any data",
+    important: false,
+    id: 2,
+  },
+];
 
-const noteReducer = (state = [], action) => {
+
+const noteReducer = (state = initialState, action) => {
+  console.log('note',action)
   switch (action.type) {
     case "NEW_NOTE":
-      return [...state, action.payload]
+      return [...state, action.payload];
 
     case "TOGGLE_IMPORTANCE": {
       const id = action.payload.id;
       const noteToChange = state.find((n) => n.id === id);
       const changedNote = {
         ...noteToChange,
-        important: !noteToChange.important
+        important: !noteToChange.important,
       };
       return state.map((note) => (note.id !== id ? note : changedNote));
     }
@@ -18,7 +32,7 @@ const noteReducer = (state = [], action) => {
   }
 };
 
-
+ 
 const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
 export const createNote = (content) => {
@@ -35,8 +49,8 @@ export const createNote = (content) => {
 export const toggleImportanceOf = (id) => {
   return {
     type: "TOGGLE_IMPORTANCE",
-    payload: { id }
-  }
-}
+    payload: { id },
+  };
+};
 
 export default noteReducer;
