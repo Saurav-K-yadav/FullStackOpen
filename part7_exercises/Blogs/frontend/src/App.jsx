29fb5780454a23b2fc23e25d9e1593f15b51aabb
Saurav Kumar Yadav <sauravkumaryadav100@gmail.com>
2024-04-05 11:37:49 +0530

    fixed menu

--- part7_exercises/Blogs/frontend/src/App.jsx
+++ part7_exercises/Blogs/frontend/src/App.jsx
@@ -3,29 +3,37 @@ import Home from './home';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import UsersDisplay from './components/users';
 import { Container } from '@mui/material';
-import UserId from './components/UserId'
+import UserId from './components/UserId';
 import Blogs from './components/Blogs';
+import { useSelector } from 'react-redux';
+
 const App = () => {
+    const user = useSelector((state) => state.user);
     const padding = {
         padding: 5,
     };
     return (
         <Container>
             <Router>
-                <div>
-                    <Link style={padding} to="/">
-                        Home
-                    </Link>
-                    <Link style={padding} to="/users">
-                        users
-                    </Link>
-                </div>
+                {user.username === '' ? (
+                    ''
+                ) : (
+                    <div>
+                        <Link style={padding} to="/">
+                            Home
+                        </Link>
+                        <Link style={padding} to="/users">
+                            users
+                        </Link>
+                    </div>
+                )}
+
 
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/users" element={<UsersDisplay />} />
                     <Route path="/users/:id" element={<UserId />} />
-                    <Route path='/blogs/:id' element={<Blogs/> } />
+                    <Route path="/blogs/:id" element={<Blogs />} />
                 </Routes>
             </Router>
         </Container>
